604d2a29ab39533ea58f667b8e18dc1f0995dd0b
pandoramaioky-creator <pandoramaioky@gmail.com>
2025-09-16 03:15:19 -0300

    feat: IA integrada com restrições de cobrança + modo ADM

--- .next/static/oH_kgLBgj1-q7HtDXGHy_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/341-c998cca4b50407a5.js","static/chunks/pages/index-ceaccb9c27251247.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
